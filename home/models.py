d2d8245f0f0e537c33a82b620b9d08cdc153fa47
chriswillphoto <christophernekich@gmail.com>
2018-04-22 19:01:09 +1000

    added menu functionality

--- home/models.py
+++ home/models.py
@@ -5,9 +5,11 @@ from wagtail.core.fields import RichTextField
 from wagtail.admin.edit_handlers import FieldPanel
 
 class HomePage(Page):
+    subheading = models.CharField(max_length=250, blank=True)
     intro_about = RichTextField(blank=True)
 
     content_panels = Page.content_panels + [
-        FieldPanel('intro_about', classname="full"),
+        FieldPanel('subheading'),
+        FieldPanel('intro_about', classname="full")
     ]
 
--- tashtalevski/settings/base.py
+++ tashtalevski/settings/base.py
@@ -28,6 +28,9 @@ INSTALLED_APPS = [
     'search',
     'blog',
 
+    'wagtailmenus',
+    'wagtail.contrib.modeladmin',
+
     'wagtail.contrib.forms',
     'wagtail.contrib.redirects',
     'wagtail.embeds',
@@ -79,6 +82,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'wagtailmenus.context_processors.wagtailmenus',
             ],
         },
     },
